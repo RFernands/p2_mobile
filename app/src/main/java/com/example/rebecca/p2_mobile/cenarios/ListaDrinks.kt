dd0c36fbee128d5d3adbcf5b865b2c821c1565ca
Rebecca <rebecca.fernands@gmail.com>
2018-12-05 00:19:51 -0200

    Criação da classe Drink, do adapter e do glide

--- app/src/main/java/com/example/rebecca/p2_mobile/cenarios/ListaDrinks.kt
+++ app/src/main/java/com/example/rebecca/p2_mobile/cenarios/ListaDrinks.kt
@@ -2,12 +2,22 @@ package com.example.rebecca.p2_mobile.cenarios
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import com.example.rebecca.p2_mobile.R
+import com.example.rebecca.p2_mobile.entidades.Drink
+import kotlinx.android.synthetic.main.activity_lista_drinks.*
 
 class ListaDrinks : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_drinks)
+    }
+
+    fun exibeLista(list: List<Drink>) {
+
+        val adapter = ListaDrinksAdapter(this, list)
+        rvDrinks.adapter = adapter
+        rvDrinks.layoutManager = LinearLayoutManager(this)
     }
 }
---
+++ app/src/main/java/com/example/rebecca/p2_mobile/cenarios/ListaDrinksAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.rebecca.p2_mobile.cenarios
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.rebecca.p2_mobile.entidades.Drink
+import com.example.rebecca.p2_mobile.utilitarios.GlideApp
+import com.example.rebecca.p2_mobile.R
+import kotlinx.android.synthetic.main.item_drink.view.*
+
+class ListaDrinksAdapter(val context: Context, val drinks: List<Drink>)
+    : RecyclerView.Adapter<ListaDrinksAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_drink, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return drinks.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindView(context, drinks[position])
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bindView(context: Context, drink: Drink) {
+            itemView.tvName.text = drink.strDrink
+
+            GlideApp.with(context)
+                .load(drink.strDrinkThumb)
+                .placeholder(R.drawable.ic_local_bar_black_24dp)
+                .centerCrop()
+                .into(itemView.imgDrink)
+        }
+
+    }
+}
